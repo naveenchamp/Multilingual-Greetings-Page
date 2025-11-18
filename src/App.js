a9d0da001128f054eafe2374927c4270b457d639
naveenchamp <naveenreddytippasani7@gmail.com>
2025-11-18 10:42:46 +0000

    greetingPage-Initial Commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import {Component} from 'react'
+import './App.css'
+
+const languageGreetingsList = [
+  {
+    id: 'bfdf40eb-eec9-4a66-a493-752fe689f0d0',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/english-greetings-img.png',
+    buttonText: 'English',
+    imageAltText: 'english',
+  },
+  {
+    id: '0ceda891-2a0c-49e2-8c62-68e78180bac6',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/tamil-greetings-img.png',
+    buttonText: 'Tamil',
+    imageAltText: 'tamil',
+  },
+  {
+    id: '89537778-7a46-4c58-988c-0adc931d087c',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/telugu-greetings-img.png',
+    buttonText: 'Telugu',
+    imageAltText: 'telugu',
+  },
+]
+
+class App extends Component {
+  state = {
+    activeLang: languageGreetingsList[0],
+  }
+
+  onChangeLanguage = lang => {
+    this.setState({activeLang: lang})
+  }
+
+  render() {
+    const {activeLang} = this.state
+    const {imageUrl, imageAltText} = activeLang
+
+    return (
+      <div className="app-container">
+        <h1 className="heading">Multilingual Greetings</h1>
+
+        <ul className="buttons-container">
+          {languageGreetingsList.map(eachLang => (
+            <li key={eachLang.id}>
+              <button
+                type="button"
+                className={`lang-button ${
+                  eachLang.id === activeLang.id ? 'active' : ''
+                }`}
+                onClick={() => this.onChangeLanguage(eachLang)}
+              >
+                {eachLang.buttonText}
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        <div className="image-container">
+          <img src={imageUrl} alt={imageAltText} className="greeting-image" />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
